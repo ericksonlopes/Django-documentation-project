4c447a49b841400d80c36d309f4c89bf71e5b0ab
EricksonLopes <ofc.erickson@gmail.com>
2020-07-20 07:27:17 -0300

    Writing more views

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('<int:question_id>', detail, name='detail'),
+    path('<int:question_id>/results/', results, name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    return HttpResponse(f'Você vendo a pergunta: {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'Você está vendo os resultados da pergunta: {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Você esta vendo os votos da questão: {question_id}')
