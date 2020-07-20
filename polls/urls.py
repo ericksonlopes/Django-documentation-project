0f38c8062f45130232c6ba8b04180ecb3d7acda6
EricksonLopes <ofc.erickson@gmail.com>
2020-07-20 08:00:33 -0300

    A shortcut: get_object_or_404()

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('<int:question_id>', detail, name='detail'),
+    path('<int:question_id>/', detail, name='detail'),
     path('<int:question_id>/results/', results, name='results'),
     path('<int:question_id>/vote/', vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from .models import *
 
 
@@ -14,11 +15,14 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('A Pergunta não existe')
-    return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls.html', {'question': question})
+
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('A Pergunta não existe')
+    # return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
